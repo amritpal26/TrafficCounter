25af31f9551f0d63158a109be6ba5999debafffa
Amritpal Singh <amritpal.am26@gmail.com>
2020-01-12 23:38:15 -0800

    added verhicle counter

--- detection_pipeline.py
+++ detection_pipeline.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy as np
 
+import utils
+
 
 # ============================================================================
 DEFAULT_MIN_SIZE_CONTOUR = 25
@@ -99,9 +101,127 @@ class VehicleDetection(PipelineProcessor):
 
         context['vehicles'] = self.detect_vehicles(fg_mask, self.min_size_contour)
         context['fg_mask'] = fg_mask
+        cv2.imshow('fg_mask', fg_mask)
         
         return context
 
+class VehicleCounter(PipelineProcessor):
+    '''
+        Count the number of vehicles entering the exit zones.
+
+        For this vehicle detection will find the centeroids of 
+        detected vehicles and we will use that information to count
+        the number of vehicles entering the exit zones.
+    '''
+
+    def __init__(self, exit_masks=[], path_size=10, max_dist_lanes=10):
+        super(VehicleCounter, self).__init__()
+        self.vehicle_count = 0
+        self.exit_masks = exit_masks
+        self.path_size = path_size
+        self.max_dist_lanes = max_dist_lanes
+
+        self.paths = []
+
+    def check_vehicle_exit(self, centroid):
+        for exit_mask in self.exit_masks:
+            try:
+                if exit_mask[centroid[1]][centroid[0]] == 255:
+                    return True
+            except:
+                return True
+        return False
+
+    def __call__(self, context):
+        context['exit_masks'] = self.exit_masks
+        context['vehicle_count'] = self.vehicle_count
+        context['paths'] = self.paths
+
+        vehicles = context['vehicles']
+        if not vehicles:
+            return context
+
+        centroids = np.array(vehicles)[:,1]
+        centroids = centroids.tolist()
+        
+        if not self.paths:
+            for centroid in centroids:
+                self.paths.append([centroid])
+        else:
+            '''
+                If the centroid is not empty keep only path_size centroids.
+                use the distance to find the vehicles crossing the exit zones.
+                
+                From all the centroids the one that matches the existing path
+                is the one that is closest to the previous path point on x-axis.
+            '''
+            new_paths = []
+            
+            for path in self.paths:
+                min_dist_x_axis = 99999999
+                match = None
+                for centroid in centroids:
+                    if len(path) == 1:
+                        d = utils.distance(centroid, path[-1])
+                    else:
+                        predicted_next_x = 2 * path[-1][0] - path[-2][0]
+                        predicted_next_y = 2 * path[-1][1] - path[-2][1]
+                        d = utils.distance(centroids[0], (predicted_next_x, predicted_next_y)) 
+
+                    if d < min_dist_x_axis:
+                        min_dist_x_axis = d
+                        match = centroid
+                
+                if match and min_dist_x_axis <= self.max_dist_lanes:
+                    centroids.remove(centroid)
+                    path.append(centroid)
+                    new_paths.append(path)
+                
+                if not match:
+                    new_paths.append(path)
+            self.paths = new_paths
+
+            # Add the points that had min_dist_x_axis > self.max_dist_lanes
+            # and which have not crossed the exit zones
+            for centroid in centroids:
+                if not self.check_vehicle_exit(centroid[0]):
+                    self.paths.append([centroid])
+            
+            for i, path in enumerate(self.paths):
+                if len(path) > self.path_size:
+                    self.paths[i] = path[:self.path_size]
+
+            # count vehicles and drop counted pathes:
+            new_paths = []
+            for i, path in enumerate(self.paths):
+                print(self.paths)
+                d = path[-2:]
+                # need at list two points to count, last point should be in 
+                # exit zone, second last point should not be in exist zone. 
+                if (
+                    len(d) >= 2
+                    and not self.check_vehicle_exit(d[0]) 
+                    and self.check_vehicle_exit(d[1])
+                ):
+                    self.vehicle_count += 1
+                    print(d)
+                else:
+                    # prevent linking with path that already in exit zone
+                    add = True
+                    # for p in path:
+                        # if self.check_vehicle_exit(p[1]):
+                        #     add = False
+                        #     print('nooo')
+                        #     break
+                    if add:
+                        new_paths.append(path)
+
+            self.paths = new_paths
+
+        context['vehicle_count'] = self.vehicle_count
+        context['paths'] = self.paths
+        return context
+  
 class Visualizer(PipelineProcessor):
 
     def __init__(self):
@@ -129,7 +249,6 @@ class Visualizer(PipelineProcessor):
         return frame
 
     def draw_ui(self, frame, vehicle_count, exit_masks):
-        
         # this just add green mask with opacity to the image.
         # create a mask with the mask color and and with the mask area.
         for exit_mask in exit_masks:
@@ -139,7 +258,7 @@ class Visualizer(PipelineProcessor):
             cv2.addWeighted(mask, 1, frame, 1, 0, frame)
 
         # drawing top block with counts
-        cv2.rectangle(frame, (0, 0), (frame.shape[1], 50), (0, 0, 0), cv2.FILLED)
+        cv2.rectangle(frame, (0, 0), (frame.shape[1], 40), (0, 0, 0), cv2.FILLED)
         cv2.putText(frame, ("Vehicles passed: {count} ".format(count=vehicle_count)), (30, 30),
         cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 1)
         
--- traffic_counter.py
+++ traffic_counter.py
@@ -1,42 +1,66 @@
 import cv2
 import numpy as np
 
-from detection_pipeline import (PipelineRunner, VehicleDetection, Visualizer)
+from detection_pipeline import (
+    PipelineRunner, 
+    VehicleDetection, 
+    VehicleCounter,
+    Visualizer
+)
 
 
 #=================================================================
 VIDEO_SOURCE = 'traffic.mp4'
-TRAINING_VIDEO = 'traffic.mp4'
+TRAINING_VIDEO = 'training_video.mp4'
 EXIT_PTS = np.array([
     [[732, 720], [732, 590], [1280, 500], [1280, 720]],     # right side
     [[0, 400], [645, 400], [645, 0], [0, 0]]                # left side
 ])
 SHAPE_VIDEO = (720,1280)
+
+# VIDEO_SOURCE = 'video.avi'
+# TRAINING_VIDEO = 'video.avi'
+# EXIT_PTS = np.array([
+#     [[260,0], [320, 0], [320, 176], [260, 174]]
+# ])
+# SHAPE_VIDEO = (176,320)
 #=================================================================
 
-def train_model(bg_subtraction, iterations=100):
-    pass
-    
+def train_model(bg_subtractor, iterations=100):
+
+    capture = cv2.VideoCapture(TRAINING_VIDEO)
+    while capture.isOpened() and iterations > 0:
+        ret, frame = capture.read()
+        if ret:
+            bg_subtractor.apply(frame, None, 0.05)
+            iterations -= 1
+
+            key = cv2.waitKey(100)
+            if key == ord('q'):
+                break
+
+    return bg_subtractor
 
 def main():
-
     # create exit masks for the exit parts for counting vehicles.
     # exit mask is VIDEO_SHAPE * 3 (pixels).
     base = np.zeros(SHAPE_VIDEO + (3,), dtype='uint8')
     exit_masks = cv2.fillPoly(base, EXIT_PTS, (255, 255, 255))[:, :, 0]
 
-    bg_subtractor = cv2.createBackgroundSubtractorKNN()
+    # Create background subtraction and add train it.
+    bg_subtractor = cv2.createBackgroundSubtractorMOG2(history=100, detectShadows=False)
+    bg_subtractor = train_model(bg_subtractor, 100)
 
     # Create a pipeline for processing the frames.
     pipeline = PipelineRunner([
         VehicleDetection(bg_subtractor),
+        VehicleCounter([exit_masks]),
         Visualizer()
     ])
 
     capture = cv2.VideoCapture(VIDEO_SOURCE)
-
-    _frame_number = -1      # real frame number 
-    frame_number = -1       # frame number sent to the context
+    _frame_number = -1                      # real frame number 
+    frame_number = -1                       # frame number sent to the context
     while capture.isOpened():
         ret, frame = capture.read()
         if ret:
@@ -50,16 +74,12 @@ def main():
             pipeline.set_context({
                 'frame': frame,
                 'frame_number': frame_number,
-                'exit_masks': [exit_masks],
-                'vehicle_count': 0
             })
             
             pipeline.run()
-
             key = cv2.waitKey(100)
             if key == ord('q'):
                 break
-
 
 #=================================================================
 
